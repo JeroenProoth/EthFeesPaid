d3656d50bc973bc4fb93211507e0d94481ec9cec
ruwan <ruwan.lambrichts@gmail.com>
2020-12-15 23:19:02 +0100

    Use params attribute of requests.Session

--- etherscan/account.py
+++ etherscan/account.py
@@ -5,31 +5,27 @@ class Account(Client):
 
     def __init__(self, address, api_key = ''):
         super().__init__(address, api_key = api_key)
-        self.url_dict[self.MODULE] = 'account'
+        self.set_query_param(module='account')
 
     def get_balance(self) -> int:
         '''Get the latest balance on address.'''
-        self.url_dict[self.ACTION] = 'balance'
-        self.url_dict[self.TAG] = 'latest'
+        self.set_query_param(action='balance')
+        self.set_query_param(tag='latest')
 
-        self.build_url()
         req = self.connect()
         return req['result']
 
     def get_transaction_page(self) -> list:
         '''Get the latest transactions on address'''
-        self.url_dict[self.ACTION] = 'txlist'
-        self.url_dict[self.SORT] = 'desc'
+        self.set_query_param(action='txlist')
+        self.set_query_param(sort='desc')
 
-        self.build_url()
         req = self.connect()
         return req['result']
 
     def get_token_balance(self, contract_address):
-        self.url_dict[self.ACTION] = 'tokenbalance'
-        self.url_dict[self.CONTRACT_ADDRESS] = contract_address
+        self.set_query_param(action='tokenbalance')
+        self.set_query_param(contactaddress=contract_address)
 
-        self.build_url()
-        print(self.url)
         req = self.connect()
         return req['result']
--- etherscan/stats.py
+++ etherscan/stats.py
@@ -3,10 +3,10 @@ from .client import Client
 class Stats(Client):
     def __init__(self, address = '', api_key = ''):
         super().__init__(address, api_key = api_key)
-        self.url_dict[self.MODULE] = 'stats'
+        self.set_query_param(module='stats')
 
     def get_ether_last_price(self):
-        self.url_dict[self.ACTION] = 'ethprice'
-        self.build_url()
+        self.set_query_param(action='ethprice')
+
         req = self.connect()
         return req['result']
