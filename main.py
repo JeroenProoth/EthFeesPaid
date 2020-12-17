0408502e0e37792efb93fb387194ab3517b0aa40
Jeroen Prooth <33283073+JeroenProoth@users.noreply.github.com>
2020-12-17 11:08:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@
 from secrets import API_KEY, ADDRESS, ADDRESS_ALT, ORN_CONTRACTADDRESS
 from calculator import Calculator
 
-from ethexplorer.client import Client
-
 if __name__ == "__main__":
     calc = Calculator(ADDRESS, API_KEY)
     print(calc.api_methods.get_tx_info(ADDRESS))
